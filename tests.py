fe97ffcd6cdd6323fb634e3722fc5fe335d0ce88
Jason Battle <jason.battle@gmail.com>
2016-05-26 22:50:19 +0900

    Update tests.py
    * Added newline at end of file for cleaner viewing.

--- tests.py
+++ tests.py
@@ -25,3 +25,4 @@ if __name__ == "__main__":
     gpiodathi = test.ReadGPIO(handle)
     test.CloseChannel(handle) # Free channel at specified handle 
     test.Cleanup_libMPSSE() # Graceful exit
+    
