1e1f3d339f8216b51f6f7b11a692c92044ab4fca
Jason Battle <jason.battle@gmail.com>
2016-06-16 20:55:19 +0900

    Update MPSSE_tests.py
    * Updated tests to reflect module changes

--- MPSSE_tests.py
+++ MPSSE_tests.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 """MPSSE_tests.py: Simple test routine for pyMPSSE wrapper functions
-
 __author__ = "Jason M. Battle"
 __copyright__ = "Copyright 2016, Jason M. Battle"
 __license__ = "MIT"
@@ -13,16 +12,16 @@ if __name__ == "__main__":
     
     test = I2CMaster()
     test.Init_libMPSSE() # Graceful initialization
-    numchan = test.GetNumChannels()
-    chaninfo = test.GetChannelInfo(0) # Channel index starts at 0
-    handle = test.OpenChannel(0) # Assign handle to channel 0
-    test.InitChannel(handle, mode='Standard') # Configure I2C port in 100kHz mode
-    test.DeviceWrite(handle, 0x71, 0x00, []) # Write register address 0x00 to device address 0x71 (R/W bit is omitted!)
-    i2cdat = test.DeviceRead(handle, 0x71, 0x07, [], 2) # Read two bytes from register address 0x07 (device address 0x71)
-    test.WriteGPIO(handle, 255, 0) # Set all GPIO pins to drive low
-    gpiodatlo = test.ReadGPIO(handle)
-    test.WriteGPIO(handle, 255, 255) # Set all GPIO pins to drive high
-    gpiodathi = test.ReadGPIO(handle)
-    test.CloseChannel(handle) # Free channel at specified handle 
+    test.GetNumChannels()
+    test.GetChannelInfo() # Channel index starts at 0
+    test.OpenChannel() # Assign handle to channel 0
+    test.InitChannel('Standard') # Configure I2C port in 100kHz mode
+    test.DeviceWrite(0x71, 0x00, []) # Write register address 0x00 to device address 0x71 (R/W bit is omitted!)
+    i2cdat = test.DeviceRead(0x71, 0x07, 2) # Read two bytes from register address 0x07 (device address 0x71)
+    test.WriteGPIO(255, 0) # Set all GPIO pins to drive low
+    gpiodatlo = test.ReadGPIO()
+    test.WriteGPIO(255, 255) # Set all GPIO pins to drive high
+    gpiodathi = test.ReadGPIO()
+    test.CloseChannel() # Free channel at specified handle 
     test.Cleanup_libMPSSE() # Graceful exit
     
